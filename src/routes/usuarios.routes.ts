27797cce2d6939efc02ee5f08e0ad7a2bb0d61d7
David <davidloaiza13@gmail.com>
2023-06-16 15:45:25 -0500

    [update] clean test commments

--- src/routes/usuarios.routes.ts
+++ src/routes/usuarios.routes.ts
@@ -7,16 +7,9 @@ router.get("/", controllers.Usuarios.getUsuarios);
 router.get("/:username", controllers.Usuarios.getUsuario);
 router.post("/", controllers.Usuarios.postUsuario);
 
-//router.delete("/:username", controllers.Usuarios.deleteUsuario);
+router.delete("/:username", controllers.Usuarios.deleteUsuario);
 router.delete("/delete/:_id", controllers.Usuarios.deleteUserById);
 router.put("/update/:_id", controllers.Usuarios.updateUsuarioById);
 router.put("/:username", controllers.Usuarios.updateUsuario);
-
-
-// router.get("/", controllers.Usuarios.getUsuarios);
-// router.get("/:username", controllers.Usuarios.getUsuario);
-// router.post("/", controllers.Usuarios.postUsuario);
-// router.put("/", controllers.Usuarios.updateUser);
-// router.delete("/:username", controllers.Usuarios.deleteUsuario);
 
 export { router };
